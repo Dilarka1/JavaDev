e0241a5e20bab0c38e9786c7f262816aa7152622
Dilara Khafizov <dilara_khafizova@gmail.com>
2020-10-20 20:33:12 -0400

    new day

---
+++ Spring-02-DI_ConstuctorInjection/src/main/java/com/cybertek/services/Java.java
@@ -0,0 +1,28 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
+
+public class Java implements Course {
+
+    //OfficeHours officeHours;
+    ExtraSessions extraSessions;
+
+
+//    public Java(OfficeHours officeHours) {
+//        this.officeHours = officeHours;
+//    }
+//
+//    @Override
+//    public void getTeachingHours() {
+//        System.out.println("Weekly Teaching Hours : " + (20 + officeHours.getHours()));
+
+    public Java(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
+
+    @Override
+    public void getTeachingHours() {
+        System.out.println("Weekly Teaching Hours : " + (20 + extraSessions.getHours()));
+    }
+}

687ccd3e6356609a2f11a297e623110fac384cd4
Dilara Khafizov <dilara_khafizova@gmail.com>
2020-11-10 20:42:59 -0500

    @PathVariable

--- Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
+++ Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
@@ -1,17 +1,40 @@
 package com.cybertek.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping
-    public String getRequestMapping(){
+    @RequestMapping("/")
+    public String getRequestMapping() {
 
         return "home";
     }
 
+    @RequestMapping(method = RequestMethod.GET, value = "/ozzy")
+    public String getRequestMapping2() {
+        return "home";
+    }
 
+    @RequestMapping(method = RequestMethod.POST, value = "/ozzy")
+    public String getRequestMapping3() {
+        return "home";
+    }
 
+//    @GetMapping("/spring")
+//    public String getMappingEx(){
+//        return "home";
+//    }
+
+    @PostMapping("/spring")
+    public String getMappingEx2(){
+        return "home";
+    }
+
+    @GetMapping("/home{name}")
+    public String pathVariableEx(@PathVariable("name") String name){
+        System.out.println("name is: " + name);
+        return "home";
+    }
 }

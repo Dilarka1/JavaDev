28fc124b0c5ba15b0f4fec9ca352d1361a3e3834
Dilara Khafizov <dilara_khafizova@gmail.com>
2020-11-12 16:04:29 -0500

    MVC Controller

--- Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
+++ Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
@@ -2,39 +2,47 @@ package com.cybertek.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
 @Controller
 public class HomeController {
-
     @RequestMapping("/")
-    public String getRequestMapping() {
-
+    public String getRequestMapping1(){
         return "home";
     }
-
-    @RequestMapping(method = RequestMethod.GET, value = "/ozzy")
-    public String getRequestMapping2() {
+    @RequestMapping(method= RequestMethod.GET,value = "/ozzy")
+    public String getRequestMapping2(){
         return "home";
     }
-
-    @RequestMapping(method = RequestMethod.POST, value = "/ozzy")
-    public String getRequestMapping3() {
+    @RequestMapping(method= RequestMethod.POST,value = "/ozzy")
+    public String getRequestMapping3(){
         return "home";
     }
-
-//    @GetMapping("/spring")
-//    public String getMappingEx(){
-//        return "home";
-//    }
-
+    @GetMapping("/spring")
+    public String getMappingEx(){
+        return "home";
+    }
     @PostMapping("/spring")
     public String getMappingEx2(){
         return "home";
     }
-
-    @GetMapping("/home{name}")
+    @GetMapping("/home/{name}")
     public String pathVariableEx(@PathVariable("name") String name){
-        System.out.println("name is: " + name);
+        System.out.println("name is => " +name);
+        return "home";
+    }
+    @GetMapping("/home/{name}/{email}")
+    public String pathVariableEx2(@PathVariable("name") String name, @PathVariable("email") String email){
+        System.out.println("name is => " +name);
+        System.out.println("email is => " +email);
+        return "home";
+    }
+    @GetMapping("home/course")
+    public String requestParamEx(@RequestParam("course") String course){
+        System.out.println("name is => "+course);
+        return "home";
+    }
+    @GetMapping(value ="/course")
+    public String requestParamEx2(@RequestParam(value ="name", required = false, defaultValue = "Cybertek") String course){
+        System.out.println("name is => "+course);
         return "home";
     }
 }

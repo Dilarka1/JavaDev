93499e798a5be6aae306ed2fe82587bdb4da2edf
Dilara Khafizov <dilara_khafizova@gmail.com>
2020-11-14 14:02:41 -0500

    Thymeleaf

--- Spring-08-SpringBoot-MVC-Model-View(Thymeleaf)/src/main/java/com/cybertek/controller/StudentController.java
+++ Spring-08-SpringBoot-MVC-Model-View(Thymeleaf)/src/main/java/com/cybertek/controller/StudentController.java
@@ -1,6 +1,6 @@
-
 package com.cybertek.controller;
 
+import com.cybertek.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,29 +16,36 @@ import java.util.Random;
 public class StudentController {
 
     @GetMapping("/welcome")
-    public String homePage(Model model){
+    public String homePage(Model model) {
 
-        model.addAttribute("name","Cybertek");
-        model.addAttribute("course","MVC");
+        model.addAttribute("name", "Cybertek");
+        model.addAttribute("course", "MVC");
 
         String subject = "Collections";
-        model.addAttribute("subject",subject);
+        model.addAttribute("subject", subject);
 
         //create some random studentid (0-1000) and show it in your UI
         int studentId = new Random().nextInt(1000);
-        model.addAttribute("id",studentId);
+        model.addAttribute("id", studentId);
 
         List<Integer> numbers = new ArrayList<>();
         numbers.add(4);
         numbers.add(5);
         numbers.add(7);
-        model.addAttribute("numbers",numbers);
+        model.addAttribute("numbers", numbers);
 
         //print your birthday
         LocalDate birthday = LocalDate.now().minusYears(42);
-        model.addAttribute("birthday",birthday);
+        model.addAttribute("birthday", birthday);
 
+        Student student = new Student(1, "Mike", "Smith");
+        model.addAttribute("student", student);
 
         return "student/welcome";
+    }
+
+    @GetMapping("/register")
+    public String homePage2() {
+        return "student/register";
     }
 }
